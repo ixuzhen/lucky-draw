17c25a43f1c47ef527494caeaa79be63d445ff6e
xz <471340101@qq.com>
2023-05-14 16:24:33 +0800

    feat:第5节完成

---
+++ lottery-domain/src/main/java/cn/itedus/lottery/domain/strategy/repository/IStrategyRepository.java
@@ -0,0 +1,13 @@
+package cn.itedus.lottery.domain.strategy.repository;
+
+import cn.itedus.lottery.domain.strategy.model.aggregates.StrategyRich;
+import cn.itedus.lottery.infrastructure.po.Award;
+
+
+public interface IStrategyRepository {
+
+    StrategyRich queryStrategyRich(Long strategyId);
+
+    Award queryAwardInfo(String awardId);
+
+}
---
+++ lottery-domain/src/main/java/cn/itedus/lottery/domain/strategy/service/draw/IDrawExec.java
@@ -0,0 +1,11 @@
+package cn.itedus.lottery.domain.strategy.service.draw;
+
+import cn.itedus.lottery.domain.strategy.model.req.DrawReq;
+import cn.itedus.lottery.domain.strategy.model.res.DrawResult;
+
+
+public interface IDrawExec {
+
+    DrawResult doDrawExec(DrawReq req);
+
+}
