c2ceadd1ca21a2c9d589b1a043e41ac6cf046a36
xz <471340101@qq.com>
2023-05-15 22:40:44 +0800

    feat:第07节：简单工厂搭建发奖领域

--- lottery-common/src/main/java/cn/itedus/lottery/common/Constants.java
+++ lottery-common/src/main/java/cn/itedus/lottery/common/Constants.java
@@ -1,19 +1,18 @@
 package cn.itedus.lottery.common;
 
 /**
- * 博客：https://bugstack.cn - 沉淀、分享、成长，让自己和他人都能有所收获！
- * 公众号：bugstack虫洞栈
- * Create by 小傅哥(fustack)
- *
- * 枚举信息定义
+ * @description: 枚举信息定义
+ * @author：小傅哥，微信：fustack
+ * @date: 2021/8/28
+ * @Copyright：公众号：bugstack虫洞栈 | 博客：https://bugstack.cn - 沉淀、分享、成长，让自己和他人都能有所收获！
  */
 public class Constants {
 
     public enum ResponseCode {
         SUCCESS("0000", "成功"),
-        UN_ERROR("0001","未知失败"),
-        ILLEGAL_PARAMETER("0002","非法参数"),
-        INDEX_DUP("0003","主键冲突");
+        UN_ERROR("0001", "未知失败"),
+        ILLEGAL_PARAMETER("0002", "非法参数"),
+        INDEX_DUP("0003", "主键冲突");
 
         private String code;
         private String info;
@@ -83,7 +82,7 @@ public class Constants {
         /**
          * 未中奖
          */
-        FAIL(0,"未中奖"),
+        FAIL(0, "未中奖"),
 
         /**
          * 已中奖
@@ -93,12 +92,103 @@ public class Constants {
         /**
          * 兜底奖
          */
-        Cover(2,"兜底奖");
+        Cover(2, "兜底奖");
 
         private Integer code;
         private String info;
 
         DrawState(Integer code, String info) {
+            this.code = code;
+            this.info = info;
+        }
+
+        public Integer getCode() {
+            return code;
+        }
+
+        public void setCode(Integer code) {
+            this.code = code;
+        }
+
+        public String getInfo() {
+            return info;
+        }
+
+        public void setInfo(String info) {
+            this.info = info;
+        }
+    }
+
+    /**
+     * 发奖状态：0等待发奖、1发奖成功、2发奖失败
+     */
+    public enum AwardState {
+
+        /**
+         * 等待发奖
+         */
+        WAIT(0, "等待发奖"),
+
+        /**
+         * 发奖成功
+         */
+        SUCCESS(1, "发奖成功"),
+
+        /**
+         * 发奖失败
+         */
+        FAILURE(2, "发奖失败");
+
+        private Integer code;
+        private String info;
+
+        AwardState(Integer code, String info) {
+            this.code = code;
+            this.info = info;
+        }
+
+        public Integer getCode() {
+            return code;
+        }
+
+        public void setCode(Integer code) {
+            this.code = code;
+        }
+
+        public String getInfo() {
+            return info;
+        }
+
+        public void setInfo(String info) {
+            this.info = info;
+        }
+    }
+
+    /**
+     * 奖品类型（1:文字描述、2:兑换码、3:优惠券、4:实物奖品）
+     */
+    public enum AwardType {
+        /**
+         * 文字描述
+         */
+        DESC(1, "文字描述"),
+        /**
+         * 兑换码
+         */
+        RedeemCodeGoods(2, "兑换码"),
+        /**
+         * 优惠券
+         */
+        CouponGoods(3, "优惠券"),
+        /**
+         * 实物奖品
+         */
+        PhysicalGoods(4, "实物奖品");
+
+        private Integer code;
+        private String info;
+
+        AwardType(Integer code, String info) {
             this.code = code;
             this.info = info;
         }
