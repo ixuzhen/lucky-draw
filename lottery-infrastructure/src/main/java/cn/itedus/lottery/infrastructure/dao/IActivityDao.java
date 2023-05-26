fbde94b209000509b7a3a82aeeff227672fd7e3e
xz <471340101@qq.com>
2023-05-26 10:41:08 +0800

    feat:第10节，第11节，第12节

--- lottery-infrastructure/src/main/java/cn/itedus/lottery/infrastructure/dao/IActivityDao.java
+++ lottery-infrastructure/src/main/java/cn/itedus/lottery/infrastructure/dao/IActivityDao.java
@@ -4,8 +4,6 @@ import cn.itedus.lottery.domain.activity.model.vo.AlterStateVO;
 import cn.itedus.lottery.infrastructure.po.Activity;
 import org.apache.ibatis.annotations.Mapper;
 
-import java.util.Map;
-
 /**
  * @description: 活动基础信息表DAO
  * @author: 小傅哥，微信：fustack
@@ -16,27 +14,34 @@ import java.util.Map;
 @Mapper
 public interface IActivityDao {
 
-   /**
-    * 插入数据
-    *
-    * @param req 入参
-    */
-   void insert(Activity req);
+    /**
+     * 插入数据
+     *
+     * @param req 入参
+     */
+    void insert(Activity req);
 
-   /**
-    * 根据活动号查询活动信息
-    *
-    * @param activityId 活动号
-    * @return 活动信息
-    */
-   Activity queryActivityById(Long activityId);
+    /**
+     * 根据活动号查询活动信息
+     *
+     * @param activityId 活动号
+     * @return 活动信息
+     */
+    Activity queryActivityById(Long activityId);
 
-   /**
-    * 变更活动状态
-    *
-    * @param alterStateVO  [activityId、beforeState、afterState]
-    * @return 更新数量
-    */
-   int alterState(AlterStateVO alterStateVO);
+    /**
+     * 变更活动状态
+     *
+     * @param alterStateVO  [activityId、beforeState、afterState]
+     * @return 更新数量
+     */
+    int alterState(AlterStateVO alterStateVO);
+
+    /**
+     * 扣减活动库存
+     * @param activityId 活动ID
+     * @return 更新数量
+     */
+    int subtractionActivityStock(Long activityId);
 
 }
